38c981fc9042dcb1b63a776e805430f23ce3b953
Angel6871 <91727236+Angel6871@users.noreply.github.com>
2023-12-12 15:49:37 +0100

    added poisson ratio and loads class

--- AttachmentConfiguration.py
+++ AttachmentConfiguration.py
@@ -1,1 +1,14 @@
 #Selection of the material
+
+CompressiveLoad = #calculated
+NBeams = 8
+ActualLoad = CompressiveLoad/NBeams
+MassTwoPreviousLugs = 79.05 * 10^(-3)
+
+
+#trade_off
+Ratio = (1371/2)/ActualLoad   #sqr(load of launch)/actualLoad
+
+#Scale the lugs given the loads for the two lugs and then divide by two
+MassNewLug = Ratio*(MassTwoPreviousLugs/2)
+TotalMassLugs = MassNewLug * NBeams
