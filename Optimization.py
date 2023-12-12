5348523b6f9ab2a758b7e88b7180dad8767fef4d
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-12-12 16:43:44 +0100

    shell buckling change final

---
+++ Optimization.py
@@ -0,0 +1,37 @@
+import PropClass
+from scipy.optimize import minimize
+import InputVariables
+import numpy as np
+import ShellBuckling as sb
+import ColumnBuckling as cb
+
+def objective_function(TankClass):
+    mass = TankClass.density * (np.pi * 2 * TankClass.radius * TankClass.t_1 * (TankClass.length - 2 * TankClass.radius ) + 4 * np.pi * TankClass.radius ** 2 * TankClass.t_2)
+    return mass
+
+def constraint_equation_state(volume,temperature,Loads):
+    alpha = InputVariables.get_alpha(temperature)
+    a = InputVariables.a
+    b = InputVariables.b
+    gas_const = InputVariables.gas_constant
+    n = InputVariables.n
+    return gas_const * temperature / ( volume / n - b ) - a * alpha / ( (volume / n) ** 2 + 2 * b * volume / n - b ** 2 ) - Loads.pressure
+
+def constraint_shell_buckling(TankClass,Loads):
+    #  launch load - pressure_longitudinal < critical stress from shell buckling
+    return sb.calculate_shell_buckling(TankClass,Loads)
+
+def constraint_column_buckling(TankClass,Loads):
+    # launc
+    return cb.calculate_column_buckling_stress(TankClass,Loads) + Loads.pressure +
+
+
+
+
+constraints = [
+    {'type': 'eq', 'fun': constraint_equation_state},
+    {'type': 'ineq', 'fun': constraint_equation_state},
+]
+
+
+result = minimize(objective_function, initial_design, constraints=constraints, method='SLSQP')
