346ce0b53b3e956e1089038dd66f3b773d78929c
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-12-14 15:15:23 +0100

    shell buckling and column buckling changes

--- Optimization.py
+++ Optimization.py
@@ -7,42 +7,63 @@ import numpy as np
 import ShellBuckling as sb
 import ColumnBuckling as cb
 
-def objective_function(TankClass):
-    mass = TankClass.density * (np.pi * 2 * TankClass.radius * TankClass.t_1 * (TankClass.length - 2 * TankClass.radius ) + 4 * np.pi * TankClass.radius ** 2 * TankClass.t_2)
+tank_variables = PropClass.FuelTank(4,2, t_1 = 3*10**-3, t_2 = 3*10**-3, material= "Ti-6AI-4V")
+temperature = 273
+loads = PropClass.Loads(pressure=2.5 * 10 ** 6, launch_axial_loads= 6 * 9.81)
+volume_liquid = 0.1 #m**3
+
+
+variables2 = [tank_variables.density,
+             tank_variables.radius ,
+             tank_variables.length ,
+             tank_variables.t_1 ,
+             tank_variables.t_2,
+             temperature,
+             volume_liquid ,
+             tank_variables.TotalVolume,
+             loads.pressure]
+
+def objective_function(variables):
+    mass = variables[0] * (np.pi * 2 * variables[1] * variables[3] * (variables[2] - 2 * variables[1] ) + 4 * np.pi * variables[1] ** 2 * variables[4])
     return mass
 
-def constraint_equation_state(volume_liquid,temperature,Loads):
-    alpha = InputVariables.get_alpha(temperature)
+def constraint_equation_state(variables):
+    alpha = InputVariables.get_alpha(variables[5])
     a = InputVariables.a
     b = InputVariables.b
     gas_const = InputVariables.gas_constant
     n = InputVariables.n
-    return gas_const * temperature / ( volume_liquid / n - b ) - a * alpha / ( (volume_liquid / n) ** 2 + 2 * b * volume_liquid / n - b ** 2 ) - Loads.pressure
+    return gas_const * variables[5] / ( variables[6] / n - b ) - a * alpha / ( (variables[6] / n) ** 2 + 2 * b * variables[6] / n - b ** 2 ) - variables[8]
 
-def constraint_shell_buckling(TankClass, Loads):
+def constraint_shell_buckling(variables):
+    poissoin_ratio = tank_variables.PoissonRatio
+    youngs_modulus = tank_variables.YoungsModulus
     #  launch load - pressure_longitudinal <= critical stress from shell buckling
-    return sb.calculate_shell_buckling(TankClass,Loads)
+    return sb.calculate_shell_buckling(variables[2],variables[1],variables[3],poissoin_ratio,variables[8],youngs_modulus)
 
-def constraint_column_buckling(TankClass,Loads):
+def constraint_column_buckling(variables):
+    youngs_modulus = tank_variables.YoungsModulus
     # launch load - pressure_longitudinal <= critical stress column
-    return cb.calculate_column_buckling_stress(TankClass,Loads)
-def constraint_volume(TankClass,volume_liquid):
-    volume_tank = TankClass.TotalVolume
-    return volume_tank - volume_liquid
-def constraint_temp_upper(temperature):
+    return cb.calculate_column_buckling_stress(variables[2],variables[1],variables[3],youngs_modulus)
+def constraint_volume(variables):
+    return variables[7] - variables[6]
+def constraint_temp_upper(variables):
     # temperature <= 298.15
-    return temperature - 298.15
-def constraint_temp_lower(temperature):
+    return variables[5] - 298.15
+def constraint_temp_lower(variables):
     # 263.15 <= temperature
-    return 263.15 - temperature
-def constraint_hoop_stress(TankClass, pressure):
+    return 263.15 - variables[5]
+def constraint_hoop_stress(variables):
+    yield_stress = tank_variables.YieldStress
     #sigma_yield = (pressure*R)/t_1
     #Safety factor of 1.1
-    return TankClass.YieldStress * 1.1 - (pressure*TankClass.radius)/TankClass.t_1
-def constraint_longitudinal_stress(TankClass, pressure):
+    return yield_stress * 1.1 - (variables[8]*variables[1])/variables[3]
+def constraint_longitudinal_stress(variables):
+    yield_stress = tank_variables.YieldStress
     #sigma_yield= (pressure*R)/(2*T_1)
     # Safety factor of 1.1
-    return TankClass.YieldStress * 1.1 - (pressure*TankClass.radius)/(TankClass.t_1 *2)
+    return yield_stress * 1.1 - (variables[8]*variables[1])/(variables[3]*2)
+
 constraints = [
     {'type': 'eq', 'fun': constraint_equation_state},
     {'type': 'ineq', 'fun': constraint_shell_buckling},
