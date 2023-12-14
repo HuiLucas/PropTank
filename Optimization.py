40c358527f030f47848e17582bdb5069f2e2d314
L.C. Huirne <l.huirne@outlook.com>
2023-12-14 15:46:49 +0100

    added peng robinson

--- Optimization.py
+++ Optimization.py
@@ -13,18 +13,17 @@ loads = PropClass.Loads(pressure=2.5 * 10 ** 6, launch_axial_loads= 6 * 9.81)
 volume_liquid = 0.1 #m**3
 
 
-variables2 = [tank_variables.density,
+variables2 = [loads.pressure,
              tank_variables.radius ,
              tank_variables.length ,
              tank_variables.t_1 ,
              tank_variables.t_2,
              temperature,
              volume_liquid ,
-             tank_variables.TotalVolume,
-             loads.pressure]
+             tank_variables.TotalVolume]
 
 def objective_function(variables):
-    mass = variables[0] * (np.pi * 2 * variables[1] * variables[3] * (variables[2] - 2 * variables[1] ) + 4 * np.pi * variables[1] ** 2 * variables[4])
+    mass = 1000 * tank_variables.density * (np.pi * 2 * variables[1] * variables[3] * (variables[2] - 2 * variables[1] ) + 4 * np.pi * variables[1] ** 2 * variables[4])
     return mass
 
 def constraint_equation_state(variables):
@@ -33,13 +32,13 @@ def constraint_equation_state(variables):
     b = InputVariables.b
     gas_const = InputVariables.gas_constant
     n = InputVariables.n
-    return gas_const * variables[5] / ( variables[6] / n - b ) - a * alpha / ( (variables[6] / n) ** 2 + 2 * b * variables[6] / n - b ** 2 ) - variables[8]
+    return gas_const * variables[5] / ( variables[6] / n - b ) - a * alpha / ( (variables[6] / n) ** 2 + 2 * b * variables[6] / n - b ** 2 ) - variables[0]
 
 def constraint_shell_buckling(variables):
     poissoin_ratio = tank_variables.PoissonRatio
     youngs_modulus = tank_variables.YoungsModulus
     #  launch load - pressure_longitudinal <= critical stress from shell buckling
-    return sb.calculate_shell_buckling(variables[2],variables[1],variables[3],poissoin_ratio,variables[8],youngs_modulus)
+    return sb.calculate_shell_buckling(variables[2],variables[1],variables[3],poissoin_ratio,variables[0],youngs_modulus)
 
 def constraint_column_buckling(variables):
     youngs_modulus = tank_variables.YoungsModulus
@@ -57,12 +56,12 @@ def constraint_hoop_stress(variables):
     yield_stress = tank_variables.YieldStress
     #sigma_yield = (pressure*R)/t_1
     #Safety factor of 1.1
-    return yield_stress * 1.1 - (variables[8]*variables[1])/variables[3]
+    return yield_stress * 1.1 - (variables[0]*variables[1])/variables[3]
 def constraint_longitudinal_stress(variables):
     yield_stress = tank_variables.YieldStress
     #sigma_yield= (pressure*R)/(2*T_1)
     # Safety factor of 1.1
-    return yield_stress * 1.1 - (variables[8]*variables[1])/(variables[3]*2)
+    return yield_stress * 1.1 - (variables[0]*variables[1])/(variables[3]*2)
 
 constraints = [
     {'type': 'eq', 'fun': constraint_equation_state},
@@ -78,6 +77,7 @@ constraints = [
 # for guess in initial_guesses:
 #     result = minimize(objective_function, guess, constraints=constraints, method='SLSQP')
 #     print(result)
-
-result = scipy.optimize.shgo(objective_function, constraints=constraints, method='SLSQP')
+bounds = [(0.0001, 100000000), (0.0001,9), (0.0001,18), (0.000001,1), (0.000001,1), (0.0001,1000), (0.0000001,100), (0.00000001,100)]
+result = minimize(objective_function,bounds=bounds, constraints=constraints, method = "SLSQP", x0 = variables2, options = {'disp': True}) #minimizer_kwargs={'method': 'SLSQP'})
 print(result)
+#print(result)
