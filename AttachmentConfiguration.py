d1f3431409eaabd55fe34e7ee108d6c1cee145b4
Angel6871 <91727236+Angel6871@users.noreply.github.com>
2023-12-19 14:04:35 +0100

    added peng robinson

--- AttachmentConfiguration.py
+++ AttachmentConfiguration.py
@@ -1,7 +1,10 @@
 #Selection of the material
 #import chemicals
 import InputVariables
-CompressiveLoad = 6*9.81*InputVariables.total_mass_sc
+def MassUpdated(MassTank):
+    TotalMass = InputVariables.total_mass_sc + MassTank
+    return TotalMass
+CompressiveLoad = 6*9.81*MassUpdated(70000) #Change mass of the tank
 NBeams = 8
 ActualLoad = CompressiveLoad/NBeams
 MassTwoPreviousLugs = 79.05 * 10**(-3)
